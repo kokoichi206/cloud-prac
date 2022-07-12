37dd268868a28a838c06602e8956f3b01106ada2
takahiro.tominaga <tt.300607@gmail.com>
2022-07-12 20:42:12 +0900

    feat: iam role for lambda (#4)

--- aws_saa/api_gateway_lambda/main.tf
+++ aws_saa/api_gateway_lambda/main.tf
@@ -4,3 +4,9 @@ module "dynamodb" {
   prefix = var.prefix
   env    = var.env
 }
+
+module "iam" {
+  source    = "./modules/iam_role"
+  prefix    = var.prefix
+  table-arn = module.dynamodb.employee_list_table.arn
+}
