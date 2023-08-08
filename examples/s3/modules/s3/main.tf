2e9cc8631570093fe0e18a3821682e51b19a37fa
takahiro.tominaga <tt.300607@gmail.com>
2023-08-08 23:46:03 +0900

    add: s3 lifecycle_configuration

--- examples/s3/modules/s3/main.tf
+++ examples/s3/modules/s3/main.tf
@@ -16,6 +16,32 @@ resource "aws_s3_bucket" "main" {
   bucket = local.bucket_name
 }
 
+resource "aws_s3_bucket_lifecycle_configuration" "main" {
+  bucket = aws_s3_bucket.main.id
+
+  rule {
+    id = "main-bucket-rule"
+
+    expiration {
+      days = 90
+    }
+
+    filter {}
+
+    status = "Enabled"
+
+    transition {
+      days          = 1
+      storage_class = "STANDARD_IA"
+    }
+
+    transition {
+      days          = 2
+      storage_class = "GLACIER"
+    }
+  }
+}
+
 resource "aws_s3_bucket_versioning" "versioning_main" {
   bucket = aws_s3_bucket.main.id
   versioning_configuration {
