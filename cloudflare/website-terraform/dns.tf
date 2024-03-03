bef9ba1380843a7a90841af4e7fee146256e33d5
kokoichi206 <tt.300607@gmail.com>
2024-03-03 18:52:53 +0900

    [terraform] cloudflare dns record proxied を有効にする

--- cloudflare/website-terraform/dns.tf
+++ cloudflare/website-terraform/dns.tf
@@ -1,8 +1,9 @@
 # Add a record to the domain
 resource "cloudflare_record" "test" {
   zone_id = var.cloudflare_zone_id
-  name    = "test"
+  name    = "home"
   value   = var.subdomain_ipv4
   type    = "A"
-  ttl     = 3600
+  proxied = true
+  ttl = 1
 }
